77be6890d36312822a75db7ad3fe2d65311669de
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2021-06-10 22:35:06 +0000

    Add compile function; fix CI

--- .env/zsh.zsh
+++ .env/zsh.zsh
@@ -21,4 +21,15 @@ up() {
   tmux send-keys -t 2 'elm-test src/*.elm --watch' C-m
 }
 
+# We compile on save, but for the first-time compilation,
+# we want to be able to do it from the shell, rather than
+# having to go and save manually each file from Emacs.
+compile() {
+  cd src
+  echo "(dolist (file argv) (message file) (find-file file) (org-babel-tangle))" > compile.el
+  emacs --script compile.el *.org
+  rm compile.el
+  cd -
+}
+
 report-custom-functions
