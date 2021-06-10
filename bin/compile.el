900359d388e4c65b649d94a9ff4ecf3dceb7334c
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2021-06-10 22:31:18 +0000

    Tangle on CI

--- bin/compile.el
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/emacs --script
-
-; TODO: Move inside the GitHub workflow, preferably
-; don't do for file in src/*.org in shell, but rather
-; in Elisp, so we don't have to start Emacs multiple
-; times.
-(find-file (elt argv 0))
-(org-babel-tangle)
