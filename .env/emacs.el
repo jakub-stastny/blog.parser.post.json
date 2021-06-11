147fea968cfdec35cec39d6cf290ef6362449a42
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2021-06-11 14:26:36 +0000

    Set theme, fix indentation

--- .env/emacs.el
+++ .env/emacs.el
@@ -7,10 +7,12 @@
 
 (my/info "Loading project-specific configuration.")
 
- (defun my/local/tangle-org-mode-files ()
-   (my/info "Running my/local/tangle-org-mode-files.")
-   (org-babel-tangle))
+(load-theme 'adwaita)
 
- (add-hook 'org-mode-hook
-   (lambda ()
-     (add-hook 'after-save-hook 'my/local/tangle-org-mode-files nil nil)))
+(defun my/local/tangle-org-mode-files ()
+  (my/info "Running my/local/tangle-org-mode-files.")
+  (org-babel-tangle))
+
+(add-hook 'org-mode-hook
+  (lambda ()
+    (add-hook 'after-save-hook 'my/local/tangle-org-mode-files nil nil)))
